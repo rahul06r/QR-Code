39424496a0c302705e212d840e96049762d6d061
Rahul <73331680+rahul06r@users.noreply.github.com>
2023-09-13 19:17:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const qrcodeContainer = document.getElementById('imgBox');
+const downloadButton = document.getElementById('downloadButton');
+
+
+function generateQRCode() {
+    const qrText = document.getElementById('qrText').value;
+
+    if (qrText.trim() === '') {
+        alert('Please enter a Name or URL');
+        return;
+    }
+
+    qrcodeContainer.innerHTML = '';
+    const qrcode = new QRCode(qrcodeContainer, {
+        text: qrText,
+        width: 150,
+        height: 150,
+        colorDark: '#0000FF',
+        colorLight: '#fff',
+        correctLevel: QRCode.CorrectLevel.H,
+    });
+
+   
+    downloadButton.style.display = 'block';
+
+}
+
+function downloadQR() {
+    if (qrcodeContainer.innerHTML) {
+        const qrCodeDataURL = qrcodeContainer.querySelector('img').src;
+        const downloadLink = document.createElement('a');
+        downloadLink.href = qrCodeDataURL;
+        downloadLink.download = 'qrcode.png';
+        downloadLink.click();
+    }
+}
